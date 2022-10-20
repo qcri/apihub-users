3a5fa2e1d6355d05194e966336f3531867a4b1a9
AhmedMahdy14 <ahmedmahdy1420@gmail.com>
2022-10-20 09:46:33 +0000

    -Fixed issue in the FK relationship. -Added check for active subscription.

--- apihub_users/security/models.py
+++ apihub_users/security/models.py
@@ -18,11 +18,14 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True)
+    email = Column(String, index=True)
     salt = Column(String)
     hashed_password = Column(String)
-    role = Column(Enum(UserType))
+    role = Column(Enum(UserType), default=UserType.USER)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.now())
     # Subscription back_populate.
     subscriptions = relationship("Subscription", back_populates="user")
+
+    def __str__(self):
+        return f"{self.username} | {self.role}"
