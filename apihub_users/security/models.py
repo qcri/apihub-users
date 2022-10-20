7cb4e6b948cbdbb620bc5b3d33c3e80e1235fc09
AhmedMahdy14 <ahmedmahdy1420@gmail.com>
2022-10-20 10:39:48 +0000

    -Prevent from adding duplicate subscriptions for the same user. -Added SubscriptionType.

--- apihub_users/security/models.py
+++ apihub_users/security/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from sqlalchemy.orm import relationship
 from sqlalchemy import (
     Boolean,
     Column,
@@ -8,8 +9,8 @@ from sqlalchemy import (
     DateTime,
     Enum,
 )
-from .schemas import UserType
 
+from .schemas import UserType
 from ..common.db_session import Base
 
 
@@ -17,10 +18,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, index=True)
     salt = Column(String)
     hashed_password = Column(String)
-    role = Column(Enum(UserType))
+    role = Column(Enum(UserType), default=UserType.USER)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.now())
+    subscriptions = relationship("Subscription", back_populates="user")
+
+    def __str__(self):
+        return f"{self.username} || {self.role}"
